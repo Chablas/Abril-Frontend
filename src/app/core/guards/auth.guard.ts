578baf53d15200f1baa2e21a2216312071125d44
Christian Alvarez <calvarez@CINMSAC.COM>
2026-02-11 17:54:35 -0500

    add deslogeo, mejora al guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,14 +1,25 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
-    const router = inject(Router);
-    const token = localStorage.getItem('access_token');
+  const router = inject(Router);
+  const authService = inject(AuthService);
 
-    if (!token) {
-        router.navigate(['/auth/login']);
-        return false;
-    }
+  const token = localStorage.getItem('access_token');
 
-    return true;
+  // Si no hay token → login
+  if (!token) {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+
+  // Si está vencido → logout + login
+  if (authService.isTokenExpired()) {
+    authService.logout(); // opcional pero recomendado
+    router.navigate(['/auth/login']);
+    return false;
+  }
+
+  return true;
 };
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,14 +4,14 @@ import { LoginRequestDTO  } from "../models/auth/login-request.model";
 import { LoginResponseDTO   } from "../models/auth/login-response.model";
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
   private readonly api = `${environment.apiUrl}api/v1/user`;
   private readonly apiUrl = `${environment.apiUrl}api/v1/auth`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   completeRegistration(data: { token: string; password: string | null | undefined }) {
     return this.http.post(`${this.api}/complete-registration`, data);
@@ -19,10 +19,10 @@ export class AuthService {
 
   login(data: LoginRequestDTO) {
     return this.http.post<LoginResponseDTO>(`${this.apiUrl}/login`, data).pipe(
-      tap(res => {
+      tap((res) => {
         localStorage.setItem('access_token', res.accessToken);
         localStorage.setItem('user', JSON.stringify(res.user));
-      })
+      }),
     );
   }
 
@@ -31,7 +31,16 @@ export class AuthService {
     localStorage.removeItem('user');
   }
 
-  isAuthenticated(): boolean {
-    return !!localStorage.getItem('access_token');
+  isTokenExpired(): boolean {
+    const token = localStorage.getItem('access_token');
+    if (!token) return true;
+
+    try {
+      const decoded: any = jwtDecode(token);
+      const exp = decoded.exp * 1000;
+      return Date.now() > exp;
+    } catch {
+      return true;
+    }
   }
 }
