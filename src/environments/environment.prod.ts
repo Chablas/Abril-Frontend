4acb13a42da95b1014ba51034ed4c8b40af04a34
Christian Alvarez <116127459+Chablas@users.noreply.github.com>
2026-02-15 20:04:11 -0500

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'abril--backend-dsctbha6g9guf8cm.chilecentral-01.azurewebsites.net/'
+    apiUrl: 'https://abril--backend-dsctbha6g9guf8cm.chilecentral-01.azurewebsites.net/'
 };
