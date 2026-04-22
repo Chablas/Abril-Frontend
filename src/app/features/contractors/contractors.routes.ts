559164aa6359e083c912fd51988039a59f7ad468
Christian Alvarez <calvarez@CINMSAC.COM>
2026-04-22 08:54:10 -0500

    fix entrado de rutas al entrar a costos

--- src/app/features/contractors/contractors.routes.ts
+++ src/app/features/contractors/contractors.routes.ts
@@ -17,6 +17,6 @@ export const CONTRACTORS_ADMIN_ROUTES: Routes = [
     loadComponent: () =>
       import('./contractor-management/components/contractor-management')
       .then(m => m.ContractorManagement),
-    data: { titulo: 'HOMOLOGACIÓN DE CONTRATISTAS' },
+    data: { titulo: 'HOMOLOGACIÓN DE CONTRATISTAS', roles: ['USUARIO DE COSTOS Y PRESUPUESTOS'] },
   }
 ];
