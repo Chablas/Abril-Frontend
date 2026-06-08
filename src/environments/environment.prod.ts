287df74d7a269b4c19fb13e284266d0c429edc4b
Christian Alvarez <calvarez@CINMSAC.COM>
2026-06-08 17:31:19 -0500

    add yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     //apiUrl: 'https://abril--backend-dsctbha6g9guf8cm.chilecentral-01.azurewebsites.net/'
-    apiUrl: 'https://abril-backend-2b1b.onrender.com/',
+    apiUrl: 'https://intranet.abril.pe/',
     azure: {
         tenantId: '53ac60b6-9b99-49d3-b52c-5899a07f865f',
         clientId: '877bf256-77a6-418d-b189-e10f6150e7e0'
